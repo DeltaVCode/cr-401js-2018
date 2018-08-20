c0dc93f81e4847b2ebe5af77b922ac0615299dd9
Keith Dahlby <keith@newbo.co>
2018-08-20 11:26:54 -0500

    Basic TCP listener!

--- 06-tcp-server/demo/chatroom/src/app.js
+++ 06-tcp-server/demo/chatroom/src/app.js
@@ -5,6 +5,20 @@ const events = require('./lib/events');
 const socketPool = require('./lib/socket-pool');
 const actions = require('./actions');
 
+const net = require('net');
+const server = net.createServer();
+
+server.on('connection', function (socket) {
+  socket.write(`Hello, world!`);
+});
+
+exports.startServer = (port) => {
+  server.listen(port, () => {
+    events.emit('start', port);
+  })
+};
+
+
 events.on('start', (portFromStartEvent) => {
   console.log(`Listening on port ${portFromStartEvent}!`);
 });
@@ -14,7 +28,3 @@ const events2 = events.events2;
 events2.on('start', (portFromStartEvent) => {
   console.log(`EVENTS2: Listening on port ${portFromStartEvent}!`);
 });
-
-exports.startServer = (port) => {
-  events.emit('start', port);
-};
