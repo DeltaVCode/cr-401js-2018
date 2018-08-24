277f963f2ac6048b79e9a8df0f45d35b6334db14
Keith Dahlby <keith@newbo.co>
2018-08-24 11:43:51 -0500

    Add better queue tests

--- 10-stacks-and-queues/demo/stacks-and-queues/__tests__/lib/queue.test.js
+++ 10-stacks-and-queues/demo/stacks-and-queues/__tests__/lib/queue.test.js
@@ -12,16 +12,31 @@ describe('queue', () => {
 
     // Act
     queue.enqueue(1);
+    expect(queue.toArray()).toEqual([1]);
+
     queue.enqueue('dos');
+    expect(queue.toArray()).toEqual([1, 'dos']);
+
     queue.enqueue({ tree: true });
+    expect(queue.toArray()).toEqual([1, 'dos', { tree: true }]);
+
+    // Uses Queue.inspect()
+    console.log(queue);
+    // Uses Queue.toString()
+    console.log(`${queue}`);
 
     // Assert
     expect(queue.count).toBe(3);
 
     // Act
     expect(queue.dequeue()).toBe(1);
+    expect(queue.toArray()).toEqual(['dos', { tree: true }]);
+
     expect(queue.dequeue()).toBe('dos');
+    expect(queue.toArray()).toEqual([{ tree: true }]);
+
     expect(queue.dequeue()).toEqual({ tree: true });
+    expect(queue.toArray()).toEqual([]);
 
     // Assert
     expect(queue.count).toBe(0);
--- 10-stacks-and-queues/demo/stacks-and-queues/lib/queue.js
+++ 10-stacks-and-queues/demo/stacks-and-queues/lib/queue.js
@@ -4,6 +4,9 @@ class Queue {
   constructor() {
     this.count = 0;
     this.values = [];
+
+    // This is silly, but tests pass
+    // this.dequeueIndex = 0;
   }
 
   enqueue(value) {
@@ -14,7 +17,32 @@ class Queue {
 
   dequeue() {
     this.count--;
+    // How would we shift without shift?
+
+    // This is silly, but tests pass
+    // return this.values[this.dequeueIndex++];
+
+    // This is a lot of code, but it works
+    // return this.values.splice(0,1)[0];
+
     return this.values.shift();
+  }
+
+  // Change how console.log() renders this
+  inspect() {
+    return this.values;
+  }
+
+  toArray() {
+    // This is silly, don't do it
+    // return this.values.slice(this.dequeueIndex);
+
+    return this.values;
+  }
+
+  // Change how queue is rendered as string
+  toString() {
+    return this.values.toString();
   }
 }
 
