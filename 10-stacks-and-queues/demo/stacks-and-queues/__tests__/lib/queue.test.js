97fb0263084af51fa7c50ac6e5768ebcfa24f674
Keith Dahlby <keith@newbo.co>
2018-08-24 11:30:05 -0500

    Basic queue array implementation

--- 10-stacks-and-queues/demo/stacks-and-queues/__tests__/lib/queue.test.js
+++ 10-stacks-and-queues/demo/stacks-and-queues/__tests__/lib/queue.test.js
@@ -3,5 +3,27 @@
 var Queue = require('../../lib/queue');
 
 describe('queue', () => {
+  it('can enqueue and dequeue in expected order', () => {
+    // Arrange
+    var queue = new Queue();
 
+    // Assert
+    expect(queue.count).toBe(0);
+
+    // Act
+    queue.enqueue(1);
+    queue.enqueue('dos');
+    queue.enqueue({ tree: true });
+
+    // Assert
+    expect(queue.count).toBe(3);
+
+    // Act
+    expect(queue.dequeue()).toBe(1);
+    expect(queue.dequeue()).toBe('dos');
+    expect(queue.dequeue()).toEqual({ tree: true });
+
+    // Assert
+    expect(queue.count).toBe(0);
+  });
 });
--- 10-stacks-and-queues/demo/stacks-and-queues/lib/queue.js
+++ 10-stacks-and-queues/demo/stacks-and-queues/lib/queue.js
@@ -1,1 +1,21 @@
 'use strict';
+
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.values = [];
+  }
+
+  enqueue(value) {
+    this.values[this.count] = value;
+    // this.values.push(value);
+    this.count++;
+  }
+
+  dequeue() {
+    this.count--;
+    return this.values.shift();
+  }
+}
+
+module.exports = Queue;
