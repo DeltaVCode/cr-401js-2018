fb560844fad43fc4184709a824519945615d50c2
JBertling <bertling.jess@gmail.com>
2018-09-17 12:17:27 -0500

    react intro demo using create-react-app

---
+++ 26-frontend-tooling-and-react/demo/my-app/src/App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div>
+        Footer Text Here
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+      polarity: 'neutral'
+    }
+    this.handleUp = this.handleUp.bind(this);
+    this.handleDown = this.handleDown.bind(this);
+    this.updateState = this.updateState.bind(this);
+  }
+
+  //handleUp() { Incorrect - Cannot modify state directly
+    //this.state.counter = this.state.counter + 1;
+  //}
+
+  handleUp() {
+    this.updateState(this.state.counter + 1);
+  }
+
+  handleDown() {
+    this.updateState(this.state.counter -1);
+  }
+
+  updateState(counter) {
+    let polarity = null;
+    if (counter > 0) { polarity = "positive" }
+    if (counter < 0) { polarity = "negative" }
+    this.setState({counter, polarity});
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <section>
+          <h2>Counter App</h2>
+          <div className="container">
+            <div className={this.state.polarity} id="counter">
+              {this.state.counter}
+            </div>
+            <a onClick={this.handleUp} href="#" id="up">Up</a>
+            <a onClick={this.handleDown} href="#" id="down">Down</a>
+          </div>
+        </section>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
