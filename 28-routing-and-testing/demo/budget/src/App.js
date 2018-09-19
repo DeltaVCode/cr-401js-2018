117ede8d3859e1e9aa8a45e0faf361a79fd2d95c
Keith Dahlby <keith@newbo.co>
2018-09-19 10:55:07 -0500

    Wire up react-router-dom

--- 28-routing-and-testing/demo/budget/src/App.js
+++ 28-routing-and-testing/demo/budget/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+
+          <header className="App-header">
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+              </ul>
+            </nav>
+          </header>
+
+          <main>
+            <Route exact path="/"
+              component={() => <h1>Home</h1>} />
+
+            <Route exact path="/dashboard"
+              component={() => <h1>Dashboard</h1>} />
+          </main>
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
