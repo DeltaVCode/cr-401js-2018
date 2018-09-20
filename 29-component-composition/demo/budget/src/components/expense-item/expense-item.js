7f0f57c9a93aeceff1166ff24749b5d0b80d7ec6
Keith Dahlby <keith@newbo.co>
2018-09-20 11:51:40 -0500

    Wire up modal open/close

--- 29-component-composition/demo/budget/src/components/expense-item/expense-item.js
+++ 29-component-composition/demo/budget/src/components/expense-item/expense-item.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 import ExpenseForm from '../expense-form/expense-form';
+import Modal from '../modal/modal';
 
 export default class ExpenseItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { showEdit: false };
+  }
+
   updateExpense = (expense) => {
     this.props.handleUpdateExpense(expense);
+  }
+
+  showEditModal = () => {
+    // NEVER UPDATE state DIRECTLY
+    // this.state.showEdit = true;
+    this.setState({
+      showEdit: true,
+    });
+  }
+
+  hideEditModal = () => {
+    this.setState({ showEdit: false });
   }
 
   render() {
@@ -12,13 +31,16 @@ export default class ExpenseItem extends Component {
     return (
       <li>
         {expense.title} : ${expense.price.toFixed(2)}
-        <div className="modal">{/* TODO */}
+
+        <button onClick={this.showEditModal}>Edit</button>
+
+        <Modal show={this.state.showEdit} handleClose={this.hideEditModal}>
           <ExpenseForm
             buttonText='Update Expense (not used)'
             expense={expense}
             handleComplete={this.updateExpense}
             />
-        </div>
+        </Modal>
       </li>
     );
   }
