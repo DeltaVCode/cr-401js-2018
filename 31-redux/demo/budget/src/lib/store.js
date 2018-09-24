95610d4f565cdd4099bbbae690e079eb50c7b27e
Keith Dahlby <keith@newbo.co>
2018-09-24 11:48:18 -0500

    Wire up basic Redux DevTools to store

--- 31-redux/demo/budget/src/lib/store.js
+++ 31-redux/demo/budget/src/lib/store.js
@@ -4,4 +4,7 @@ import { createStore } from 'redux';
 import reducer from '../reducer';
 
 export default () =>
-  createStore(reducer);
+  createStore(reducer,
+    // Wire up Redux DevTools Chrome Extension
+    // https://github.com/zalmoxisus/redux-devtools-extension
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
