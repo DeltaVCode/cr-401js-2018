73b77e074c09f414c8beccb22a0805b73ee856a4
Keith Dahlby <keith@newbo.co>
2018-09-24 11:35:26 -0500

    Finish expenses reducer (add, update)

--- 31-redux/demo/budget/src/reducer/expenses.test.js
+++ 31-redux/demo/budget/src/reducer/expenses.test.js
@@ -20,4 +20,46 @@ describe('expenses reducer', () => {
     let res = reducer(state, action);
     expect(res).toBe(state);
   });
+
+  it('adds expense', () => {
+    const state = [{ title: 'test' }];
+    const action = {
+      type: 'EXPENSE_ADD',
+      payload: { title: 'added' },
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    let res = reducer(state, action);
+    expect(res).not.toBe(state); // redundant if we use deepFreeze
+    expect(res).toEqual([
+      { title: 'test' },
+      { title: 'added' },
+    ]);
+  });
+
+  it('updates expense', () => {
+    // Arrange
+    const state = [
+      { _id: 1, title: 'uno' },
+      { _id: 2, title: 'dos' },
+    ];
+    const action = {
+      type: 'EXPENSE_UPDATE',
+      payload: { _id: 1, title: 'UNO' },
+    };
+
+    deepFreeze(state);
+    deepFreeze(action);
+
+    // Act
+    let res = reducer(state, action);
+
+    // Assert
+    expect(res).toEqual([
+      { _id: 1, title: 'UNO' },
+      { _id: 2, title: 'dos' },
+    ]);
+  })
 })
