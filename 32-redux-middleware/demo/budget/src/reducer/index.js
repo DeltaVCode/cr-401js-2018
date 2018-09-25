ae485220fc7a52ab6b21c472ddbcb74a46db2f88
Keith Dahlby <keith@newbo.co>
2018-09-25 09:47:58 -0500

    Add error reducer (needs tests!)

--- 32-redux-middleware/demo/budget/src/reducer/index.js
+++ 32-redux-middleware/demo/budget/src/reducer/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import expenses from './expenses';
+import error from './error';
 // TODO: next reducer imported here
 
 export default combineReducers({
   expenses, // sets up { expenses: [] } to be part of state
+  error,    // sets up { error: (null | Error) }
   // TODO: next reducer attached here
 });
