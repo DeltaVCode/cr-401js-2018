3cf8c7b35e1b0d5fdc1733f2a61e0f72593bee3c
Keith Dahlby <keith@newbo.co>
2018-09-25 20:41:04 -0500

    Add example of handling Category action in Expenses reducer

--- 32-redux-middleware/demo/budget/src/reducer/expenses.js
+++ 32-redux-middleware/demo/budget/src/reducer/expenses.js
@@ -11,6 +11,23 @@ export default (state = initialState, action = {}) => {
     case 'EXPENSE_UPDATE':
       return state.map(exp =>
         exp._id === payload._id ? payload : exp);
+
+    case 'CATEGORY_REMOVE':
+      // payload is a category!
+
+      // delete expenses from that category
+      return state.filter(exp =>
+          exp.categoryID !== payload._id);
+
+      // or remove references to the category
+      return state.map(exp => {
+        if (exp.categoryID === payload._id) {
+          exp = {...exp}; // clone exp to remove its category
+          exp.categoryID = null;
+        }
+        return exp;
+      }
+
     default:
       return state;
   }
