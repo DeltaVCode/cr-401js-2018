d35152e10a4f6306b5b602b56bde6263350c1168
Keith Dahlby <keith@newbo.co>
2018-10-02 10:04:10 -0500

    Upgrade to latest create-react-app

--- 37-cookies/demo/frontend/src/App.test.js
+++ 37-cookies/demo/frontend/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import storeFactory from './lib/store.js';
+const store = storeFactory();
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
